661adc2b14de70a12d0cda99576eeb1aa9723ca0
bakaruu <arucastro@gmail.com>
2026-03-25 17:55:12 +0100

    feat: initial project setup - monorepo structure and base configuration

---
+++ notification-service/src/test/java/com/bakaru/notificationservice/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bakaru.notificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
